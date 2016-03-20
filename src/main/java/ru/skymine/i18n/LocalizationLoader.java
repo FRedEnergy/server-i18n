00d543cc55b282ff04b8351aaac69759b7ac1641
RedEnergy <forwot163@gmail.com>
2016-03-20 22:10:40 +0400

    Implemented LocalizationLoader which maintains all interaction with forge and other mods

---
+++ src/main/java/ru/skymine/i18n/LocalizationLoader.java
@@ -0,0 +1,59 @@
+package ru.skymine.i18n;
+
+import com.google.common.base.Charsets;
+import net.minecraftforge.fml.common.FMLModContainer;
+import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+@Localized
+@Mod(modid = "server-i18n", acceptableRemoteVersions = "*")
+public class LocalizationLoader {
+
+    public static final String LANG_DIR_PATH = "lang/";
+    public static final File LANG_DIR = new File(LANG_DIR_PATH);
+
+    @Mod.Instance("server-i18n")
+    public static LocalizationLoader instance;
+
+    @Mod.EventHandler
+    public void load(FMLPreInitializationEvent event) throws IOException {
+        LANG_DIR.mkdir();
+        Loader.instance().getModList()
+                .stream()
+                .filter(FMLModContainer.class::isInstance)
+                .map(FMLModContainer.class::cast)
+                .map(it -> it.getMod().getClass())
+                .filter(it -> it.isAnnotationPresent(Localized.class))
+                .forEach(this::extractLocalesFrom);
+        SI18n.findAndInjectLocales();
+    }
+
+    private void extractLocalesFrom(Class clazz){
+        try {
+            List<String> files = IOUtils.readLines(clazz.getClassLoader().getResourceAsStream(LANG_DIR_PATH), Charsets.UTF_8);
+            for (String file : files) {
+                InputStream data = clazz.getClassLoader().getResourceAsStream(LANG_DIR_PATH + file);
+                File destination = new File(LANG_DIR, file);
+                if(destination.exists()) {
+                    continue;
+                } else {
+                    destination.createNewFile();
+                }
+                FileUtils.copyInputStreamToFile(data, destination);
+                data.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
