1f30cf5f62674f46158a3674d1c194060237d48d
RedEnergy <forwot163@gmail.com>
2016-03-27 14:24:09 +0400

    Lang files no longer deployed to the environment. Default locale is now configurable in localization.cfg. Fixed file mod loading

--- src/main/java/ru/skymine/i18n/LocalizationLoader.java
+++ src/main/java/ru/skymine/i18n/LocalizationLoader.java
@@ -1,57 +1,77 @@
 package ru.skymine.i18n;
 
 import com.google.common.base.Charsets;
+import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.FMLModContainer;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.compress.compressors.FileNameUtil;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Paths;
+import java.io.InputStreamReader;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.stream.Collectors;
 
 @Localized
 @Mod(modid = "server-i18n", acceptableRemoteVersions = "*")
 public class LocalizationLoader {
 
     public static final String LANG_DIR_PATH = "lang/";
-    public static final File LANG_DIR = new File(LANG_DIR_PATH);
 
     @Mod.Instance("server-i18n")
     public static LocalizationLoader instance;
 
     @Mod.EventHandler
     public void load(FMLPreInitializationEvent event) throws IOException {
-        LANG_DIR.mkdir();
-        Loader.instance().getModList()
+        Configuration config = new Configuration(new File(event.getModConfigurationDirectory(), "localization.cfg"));
+        String defLocale = config.getString("Default Locale", "Localization", "ru", "Default locale for mods");
+        config.save();
+        SI18n.setDefaultLocale(defLocale);
+        List<FMLModContainer> localized = Loader.instance().getModList()
                 .stream()
                 .filter(FMLModContainer.class::isInstance)
                 .map(FMLModContainer.class::cast)
-                .map(it -> it.getMod().getClass())
-                .filter(it -> it.isAnnotationPresent(Localized.class))
-                .forEach(this::extractLocalesFrom);
-        SI18n.findAndInjectLocales();
+                .filter(it -> it.getMod().getClass().isAnnotationPresent(Localized.class))
+                .collect(Collectors.toList());
+        localized.stream()
+                .filter(it -> it.getSource().isDirectory())
+                .forEach(it -> extractLocalesFrom(it.getMod().getClass()));
+        localized.stream()
+                .filter(it -> it.getSource().isFile())
+                .forEach(it -> extractLocalesFrom(it.getSource()));
+    }
+
+    private void extractLocalesFrom(File source){
+        try {
+            JarFile file = new JarFile(source);
+            Enumeration<JarEntry> entries = file.entries();
+            while(entries.hasMoreElements()){
+                JarEntry entry = entries.nextElement();
+                if(entry.getName().startsWith(LANG_DIR_PATH)){
+                    InputStreamReader reader = new InputStreamReader(file.getInputStream(entry));
+                    SI18n.injectLocale(FilenameUtils.getBaseName(entry.getName()), reader);
+                    reader.close();
+                }
+            }
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void extractLocalesFrom(Class clazz){
         try {
-            String modid = ((Mod)clazz.getAnnotation(Mod.class)).modid();
             List<String> files = IOUtils.readLines(clazz.getClassLoader().getResourceAsStream(LANG_DIR_PATH), Charsets.UTF_8);
             for (String file : files) {
-                InputStream data = clazz.getClassLoader().getResourceAsStream(LANG_DIR_PATH + file);
-                File destinationDir = new File(LANG_DIR + File.separator + modid);
-                File destination = new File(destinationDir, file);
-                if(destination.exists()) {
-                    continue;
-                } else {
-                    destinationDir.mkdir();
-                    destination.createNewFile();
-                }
-                FileUtils.copyInputStreamToFile(data, destination);
+                InputStreamReader data = new InputStreamReader(clazz.getClassLoader().getResourceAsStream(LANG_DIR_PATH + file));
+                SI18n.injectLocale(FilenameUtils.getBaseName(file), data);
                 data.close();
             }
         } catch (IOException e) {
--- src/main/java/ru/skymine/i18n/SI18n.java
+++ src/main/java/ru/skymine/i18n/SI18n.java
@@ -2,13 +2,11 @@ package ru.skymine.i18n;
 
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
-import org.apache.commons.compress.compressors.FileNameUtil;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 
 import java.io.*;
 import java.util.Collection;
-import java.util.List;
 
 /**
  * Server side localization
@@ -19,23 +17,6 @@ public class SI18n {
 
     private static String defaultLocale = "en";
     private static final Table<String, String, String> localizations = HashBasedTable.create();
-
-    public static void findAndInjectLocales() throws IOException {
-        findAndInjectLocales(LocalizationLoader.LANG_DIR);
-    }
-
-    public static void findAndInjectLocales(File dir) throws IOException {
-        if(dir.isDirectory() && dir.exists()){
-            Collection<File> files = FileUtils.listFiles(dir, new String[]{"lang"}, true);
-            for(File file: files){
-                FileReader reader = new FileReader(file);
-                injectLocale(FilenameUtils.getBaseName(file.getAbsolutePath()), reader);
-                reader.close();
-            }
-        } else {
-            dir.mkdir();
-        }
-    }
 
     public static void injectLocale(String locale, Reader data) throws IOException {
         BufferedReader reader = new BufferedReader(data);
